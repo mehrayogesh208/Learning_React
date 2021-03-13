3425fc8e7ef5fa24339923b379ac67898573d69e
mehrayogesh208 <mehrayogesh208@gmail.com>
2021-03-13 17:03:32 +0530

    Everything in own file

---
+++ one-helloworld/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+function App(){
+  return(
+    <div>
+    <h1 style={{textAlign:'center'}}>Hello World</h1>
+    <button className="btn">Hello World</button>
+
+    </div>
+    
+  )
+}
+
+export default App
--- one-helloworld/src/index.js
+++ one-helloworld/src/index.js
@@ -1,15 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './style.css'
-function App(){
-  return(
-    <div>
-    <h1 style={{textAlign:'center'}}>Hello World</h1>
-    <button className="btn">Click Me</button>
 
-    </div>
-    
-  )
-}
+import ReactDOM from 'react-dom'
+import App from './App'
+
 
 ReactDOM.render(<App/>,document.getElementById('root'))
