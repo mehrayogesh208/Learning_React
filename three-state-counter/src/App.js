5282136bff22800c670efc3d1bf1a9fef057de49
mehrayogesh208 <mehrayogesh208@gmail.com>
2021-03-14 19:56:55 +0530

    Counter App

--- three-state-counter/src/App.js
+++ three-state-counter/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './App.css'
 
-const App = ()=>(
-  <div>
-    <h1>
-      Hello
-    </h1>
+
+const App = ()=>{
+
+  const[count,setCount] = useState(0)
+
+  return(
+    <div className="App">
+    <header>
+      <h1>Counter app using State/hooks</h1>
+    </header>
+    <h2>
+      Current value of count is {count}
+    </h2>
+    <button onClick={()=>setCount(0)}>Reset</button>
+    <button onClick={()=>setCount(count+1)}>Increase Counter</button>
+    <button onClick={()=>setCount(count-1)}>Decrease Counter</button>
   </div>
-)
+  )
+  
+ 
+}
 
 export default App;
