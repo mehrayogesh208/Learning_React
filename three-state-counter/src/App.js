4de53cc6129a371c65329754a51a13e3b99426d6
mehrayogesh208 <mehrayogesh208@gmail.com>
2021-03-14 20:03:03 +0530

    Added range feature

--- three-state-counter/src/App.js
+++ three-state-counter/src/App.js
@@ -15,8 +15,8 @@ const App = ()=>{
       Current value of count is {count}
     </h2>
     <button onClick={()=>setCount(0)}>Reset</button>
-    <button onClick={()=>setCount(count+1)}>Increase Counter</button>
-    <button onClick={()=>setCount(count-1)}>Decrease Counter</button>
+    <button onClick={()=>count >= 10?"":setCount(count+1)}>Increase Counter</button>
+    <button onClick={()=> count>=1?setCount(count-1):""}>Decrease Counter</button>
   </div>
   )
   
