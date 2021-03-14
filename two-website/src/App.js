77060589f4fd41a6fd0476cdef6ab02bf5f94f28
mehrayogesh208 <mehrayogesh208@gmail.com>
2021-03-14 15:12:26 +0530

    convert html to react component

--- two-website/src/App.js
+++ two-website/src/App.js
@@ -1,42 +1,19 @@
 import React from 'react'
-
+import NavBar from './NavBar'
+import CardSection from './CardSection'
+import Footer from './Footer'
+import AppSection from './AppSection'
+import HeroSection from './HeroSection'
 function App(){
   return(
-   <div>
-     <nav className="navbar navbar-expand-lg text-white bg-dark">
-      <a className="navbar-brand text-success" href="#">Navbar</a>
-
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link text-white" href="#">Home </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-white" href="#">About Us</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link text-white">
-              Products
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">Meeting</a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
-    </nav>
-   </div>
+    <div>
+  <NavBar/>
+  <HeroSection/>
+   <CardSection/>
+   <AppSection/>
+   <Footer/>
+    </div>
+   
   )
 }
 
